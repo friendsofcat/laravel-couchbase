ff28c8bb299fdad847ac75066f9179367fab7be6
Rob Fonseca <robfonseca@gmail.com>
2022-04-21 06:42:04 -0400

    Adjust CI file

--- ci/couchbase/setup.sh
+++ ci/couchbase/setup.sh
@@ -6,7 +6,7 @@ echo "Install couchbase required tools for cli usage below"
 curl -O https://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-amd64.deb
 dpkg -i ./couchbase-release-1.0-amd64.deb
 apt-get update
-apt-get install -y couchbase-server=6.6.*
+apt-get install -y couchbase-server=7.0.*
 export PATH=$PATH:/opt/couchbase/bin
 
 
@@ -14,9 +14,9 @@ echo "couchbase-cli version: $(couchbase-cli --version)"
 
 # Create cluster
 echo "Creating couchbase cluster"
-couchbase-cli cluster-init -c "$host:$port" \
-          --cluster-username $username \
-          --cluster-password $password \
+couchbase-cli cluster-init -c "$COUCHBASE_HOST:$COUCHBASE_PORT" \
+          --cluster-username $COUCHBASE_USERNAME \
+          --cluster-password $COUCHBASE_PASSWORD \
           --services data,index,query,analytics \
           --cluster-ramsize 2048 \
           --cluster-index-ramsize 256
@@ -24,11 +24,11 @@ couchbase-cli cluster-init -c "$host:$port" \
 # Add new bucket
 # Can also be done using BucketCreate command from friendsofcat/laravel-couchbase
 
-echo "Creating couchbase bucket $bucket"
-couchbase-cli bucket-create -c "$host:$port" \
-        --username $username \
-        --password $password \
-        --bucket $bucket \
+echo "Creating couchbase bucket $COUCHBASE_BUCKET"
+couchbase-cli bucket-create -c "$COUCHBASE_HOST:$COUCHBASE_PORT" \
+        --username $COUCHBASE_USERNAME \
+        --password $COUCHBASE_PASSWORD \
+        --bucket $COUCHBASE_BUCKET \
         --bucket-type couchbase \
         --bucket-ramsize 1024 \
         --enable-flush 1
@@ -38,4 +38,4 @@ echo "CBQ version: $(cbq --version)"
 
 # Create index to allow n1ql queries
 echo "Creating couchbase primary index on $bucket"
-cbq -u $username -p $password -e "$host:$port" -s "CREATE PRIMARY INDEX "${bucket}_index" ON $bucket"
+cbq -u $COUCHBASE_USERNAME -p $COUCHBASE_PASSWORD -e "http://$COUCHBASE_HOST:$COUCHBASE_PORT" -s "CREATE PRIMARY INDEX "${COUCHBASE_BUCKET}_index" ON $COUCHBASE_BUCKET"
